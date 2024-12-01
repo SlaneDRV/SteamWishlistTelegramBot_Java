db78a42a1c88d7741765c197362c27ce10823d43
SlaneDRV <slane.drv@gmail.com>
2024-12-01 21:41:38 +0100

    Temp config

--- src/MainFunctions/Config.java
+++ src/MainFunctions/Config.java
@@ -3,10 +3,10 @@ package MainFunctions;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 
 public class Config {
-    public static final String TOKEN = "";
-    public static final String STEAM_KEY = "";
-    public static final long TG_ID1 = ;
-    public static final long TG_ID2 = ;
-    public static final long TG_ID3 = ;
+    public static final String TOKEN = "7169861969:AAG4g7Wba_pl-zlksDs_14N3me3XWk6LoCE";
+    public static final String STEAM_KEY = "D350BB8AC6A45C05FA8B4EF538CEAE64";
+    public static final long TG_ID1 = 710194927;
+    public static final long TG_ID2 = 710194927;
+    public static final long TG_ID3 = 710194927;
 
 }
